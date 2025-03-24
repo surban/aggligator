44f532cb9b7529b5dcc68adde667afbf190b595c
Sebastian Urban <surban@surban.net>
2025-03-24 14:12:28 +0100

    exec: remove unused code

--- aggligator/src/exec/mod.rs
+++ aggligator/src/exec/mod.rs
@@ -16,28 +16,3 @@ mod js;
 pub use js::*;
 
 pub use task::spawn;
-
-/// Whether threads are available and working on this platform.
-#[inline]
-pub fn are_threads_available() -> bool {
-    use std::sync::LazyLock;
-
-    static AVAILABLE: LazyLock<bool> = LazyLock::new(|| {
-        let res = std::thread::Builder::new().name("remoc thread test".into()).spawn(|| ());
-        match res {
-            Ok(hnd) => match hnd.join() {
-                Ok(()) => true,
-                Err(payload) => {
-                    tracing::warn!(?payload, "test thread panicked, streaming (de)serialization disabled");
-                    false
-                }
-            },
-            Err(os_error) => {
-                tracing::warn!(%os_error, "threads not available, streaming (de)serialization disabled");
-                false
-            }
-        }
-    });
-
-    *AVAILABLE
-}
